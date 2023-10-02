077f05db5e368822d360efdc73544d31f0d30b83
reeuze <adittitik@gmail.com>
2023-10-02 22:59:15 +0700

    Commit 1 (Convert input finish)

--- Postfix_to_Infix.py
+++ Postfix_to_Infix.py
@@ -1,7 +1,28 @@
 class Convert:
     def __Init__(self):
         self.Input = []
-    def Algorithm(self):
-        print("A")
+        self.Stack = []
+        self.Output = ""
+    def Print(self, scan = ""):
+        print("scanned : ", scan)
+        print("Stack : ", self.Stack)
+    def Convert(self, input):
+        for char in input:
+            self.Input.append(char)
+    def Is_Operator(self, character):
+        operators = "+-*/^"
+        if character in operators:
+            return True
+    def Algorithm(self, input = [], index = 0):
+        for i in range(index, len(input)):
+            if len(input)==1:
+                break
+            elif input[i].isdigit():
+                self.Algorithm(input, i+1)
+                print("A")
+            elif self.Is_Operator(input[i]) is True:
+                print("B")
 
+input = "123/456^*-7+8*+"
 a = Convert
+a.Convert(a.Input)
