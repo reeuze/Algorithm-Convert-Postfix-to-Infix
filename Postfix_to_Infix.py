d096919925510772f6783beda1c8314026789d90
reeuze <adittitik@gmail.com>
2023-10-04 07:25:51 +0700

    Commit 6 (Succeed)

--- Postfix_to_Infix.py
+++ Postfix_to_Infix.py
@@ -1,10 +1,11 @@
+import time
 class Convert:
     def __init__(self):
         self.Input = []
         self.Output = ""
     def Print(self, scan = ""):
         print("scanned : ", scan)
-        print("input : ", self.Input)
+        print("input : ", self.Input, '\n')
     def Convert(self, input = ""):
         for char in input:
             self.Input.append(char)
@@ -14,33 +15,24 @@ class Convert:
             return True
     def Combine(self, value_1, value_2, operator):
         result = str('('+value_1+operator+value_2+')') 
-        print(result)
         return result
     def Algorithm(self, input = [], index = 0):
         i = 0
         i += index
         while i < len(input):
             self.Print(input[i])
-            print(len(input))
-            print("index = ", i, "\n")
             if len(input)==1:
                 break
-            # elif input[i].isdigit():
-            #     again = self.Algorithm(input, i+1)
-            #     print(again)
-            #     if again is True:
-            #         return False
-            #     else:
-            #         print('\n')
             elif self.Is_Operator(input[i]) is True:
                 input[i-2] = self.Combine(input[i-2], input[i-1], input[i])
                 for j in range(0, 2):
                     input.pop(i-1)
                 i -= 2
-                # return True
             i += 1
+            time.sleep(1.5)
 
 input = "123/456^*-7+8*+"
 a = Convert()
 a.Convert(input)
 a.Algorithm(a.Input)
+print("Result : ", ''.join(a.Input))
